6b03da641e2d1841e414266cb6523fa3fa919f85
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-03-21 17:10:51 +0000

    Create session.ts

---
+++ src/lib/session.ts
@@ -0,0 +1,12 @@
+// src/lib/session.ts
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export async function getCurrentUser() {
+  const session = await getSession();
+  return session?.user ?? null;
+}
