fa9179f916d4ecce3ee4f412b6e962a8c77b6896
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-03-21 12:54:33 +0000

    Update route.ts

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,4 +1,4 @@
-import { auth } from "@/lib/auth";
+import { auth } from "../../../../lib/auth";
 import { PrismaClient } from "@prisma/client";
 import { compare } from "bcryptjs";
 import { NextResponse } from "next/server";
